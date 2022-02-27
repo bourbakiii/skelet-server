211ceab747b812ccc551a0994d36cb59f13dc7af
bourbakii <gabisov03912@gmail.com>
2022-02-27 20:11:16 +0300

    req res params body query eth.

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import express from "express";
 
 const PORT = 5000;
 
-const app = express();
-app.get('/', (req,res)=>{
+const app = express().use(express.json());
+
+app.post('/', (req,res)=>{
+    // console.log('Queries -');
+    // console.log(req.query);
+    console.log('Body');
+    console.log(req.body);
     res.status(200).json('Сервер работаfет');
 });
 app.listen(PORT, ()=>console.log('SERVER STARTED SUCCESSFULL ' + PORT));
