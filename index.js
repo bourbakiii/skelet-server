ed89abb3ce5e2f4e053b3db75895986712f89dc6
bourbakii <gabisov03912@gmail.com>
2022-02-27 19:25:51 +0300

    nodemon

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const PORT = 5000;
+
+const app = express();
+app.get('/', (req,res)=>{
+    res.status(200).json('Сервер работаfет');
+});
+app.listen(PORT, ()=>console.log('SERVER STARTED SUCCESSFULL ' + PORT));
