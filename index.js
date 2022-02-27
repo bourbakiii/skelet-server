b9b9c0f68de945d3b3ef86b9e4e44a106a60c08d
bourbakii <gabisov03912@gmail.com>
2022-02-27 20:54:45 +0300

    Mongoose & DataBase

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import Post from "./Post.js";
 const PORT = 5000;
+const DB_URL = `mongodb+srv://bourbakiii:8236251Telefon@cluster0.vqwrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const app = express().use(express.json());
 
-app.post('/', (req,res)=>{
-    // console.log('Queries -');
-    // console.log(req.query);
-    console.log('Body');
-    console.log(req.body);
-    res.status(200).json('Сервер работаfет');
+app.post("/", async (req, res) => {
+  console.log(req.body);
+  const { author, title, content, picture } = req.body;
+  const post = await Post.create({ author, title, content, picture });
+  res.status(200).json("Сервер работаfет");
 });
-app.listen(PORT, ()=>console.log('SERVER STARTED SUCCESSFULL ' + PORT));
+
+startApp();
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    app.listen(PORT, () => console.log("SERVER STARTED SUCCESSFULL " + PORT));
+  } catch (error) {
+    console.log(error);
+  }
+}
