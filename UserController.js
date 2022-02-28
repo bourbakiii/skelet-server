235a49b98445a2c866ead59e970387fbee7be601
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-02-28 19:19:56 +0300

    email unique

--- UserController.js
+++ UserController.js
@@ -1,10 +1,14 @@
 import User from "./User.js";
-import bcryptjs from 'bcryptjs';
 
 class UserController {
     async create(req, res) {
         try {
             const { image, name, surname, father_name, email, password } = req.body;
+            const founded = await User.findOne({ "email": email }).exec();
+            // Если пользователь найден возвращаем ошибку, иначе создаем его
+            if (founded) {
+              res.status(409).json({ message: 'Пользователь с таким адресом электронной почты уже зарегистрирован' });
+            }
             const user = await User.create({ image, name, surname, father_name, email, password });
             res.status(200).json(user);
         } catch (error) {
