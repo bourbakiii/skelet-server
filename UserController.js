5f99d4c0fb3b1c0948d131041e6ff6b0cbe9da30
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-02-28 19:43:37 +0300

    User login start

--- UserController.js
+++ UserController.js
@@ -1,18 +1,38 @@
+import { response } from "express";
 import User from "./User.js";
+
 
 class UserController {
     async create(req, res) {
         try {
             const { image, name, surname, father_name, email, password } = req.body;
             const founded = await User.findOne({ "email": email }).exec();
-            // Если пользователь найден возвращаем ошибку, иначе создаем его
             if (founded) {
-              res.status(409).json({ message: 'Пользователь с таким адресом электронной почты уже зарегистрирован' });
+                res.status(409).json({ message: 'Пользователь с таким адресом электронной почты уже зарегистрирован' });
             }
             const user = await User.create({ image, name, surname, father_name, email, password });
             res.status(200).json(user);
         } catch (error) {
             console.log("User create error:");
+            console.log(error);
+            res.status(500).json(error);
+        }
+    }
+     login(req, res) {
+        try {
+            const { email, password } = req.body;
+            const the_user =  User.find({ "email":email });
+            console.log(the_user.schema.object);
+            // if(the_user){
+            //     console.log(the_user.email);
+            // }
+            // else{
+            //     console.log("Почему не работате");
+            // }
+            // const user = await User.login({ email, password });
+            // res.status(200).json(user);
+        } catch (error) {
+            console.log("User login error:");
             console.log(error);
             res.status(500).json(error);
         }
--- user_router.js
+++ user_router.js
@@ -4,7 +4,7 @@ import UserController from "./UserController.js";
 const router = new Router();
 
 router.post("/users", UserController.create);
-// router.get("/products", ProductController.getAll);
+router.get("/user", UserController.login);
 // router.get("/products/:id", ProductController.getOne);
 // router.put("/products", ProductController.update);
 // router.delete("/products/:id", ProductController.delete);
