2095b015a34fd24b9e97f015870a02588b39aa6c
bourbakii <gabisov03912@gmail.com>
2022-03-10 19:09:09 +0300

    Control point

--- product/ProductController.js
+++ product/ProductController.js
@@ -1,10 +1,20 @@
 import Product from "./Product.js";
+import Category from "../category/Category.js";
 
 class ProductController {
   async create(req, res) {
     try {
-      const { image, name, description, price, discount_price, active, stock } = req.body;
-      const product = await Product.create({ image, name, description, price, discount_price, active, stock });
+      const { image, name, description, price, discount_price, active, stock } =
+        req.body;
+      const product = await Product.create({
+        image,
+        name,
+        description,
+        price,
+        discount_price,
+        active,
+        stock,
+      });
       res.status(200).json(product);
     } catch (error) {
       console.log("Product create error:");
@@ -15,9 +25,9 @@ class ProductController {
   async getAll(req, res) {
     try {
       const products = await Product.find();
-      products.forEach(async element => {
-        // !!! При возвращении чтобы сами категории раскрывались
-      });
+      let categories = new Array();
+      await products.forEach(element=>{
+      })
       return res.json(products);
     } catch (error) {
       res.status(500).json(error);
@@ -26,7 +36,8 @@ class ProductController {
   async getOne(req, res) {
     try {
       const { id } = req.params;
-      if (!id) return res.status(500).json({ message: 'ID продукта не указан' });
+      if (!id)
+        return res.status(500).json({ message: "ID продукта не указан" });
       const products = await Product.findById(id);
       return res.json(products);
     } catch (error) {
@@ -34,25 +45,29 @@ class ProductController {
     }
   }
   async update(req, res) {
-        try {
-            const product = req.body;
-            if(!product.id)  return res.status(404).json({message:'ID продукта не указан'});
-            const updatedProduct = await Post.findByIdAndUpdate(product.id, product, {new:true});
-            return res.json(updatedProduct);
-        } catch (error) {
-          res.status(500).json(error);
-        }
-      }
-      async delete(req, res) {
-        try {
-            const {id} = req.params;
-            if(!id) return res.status(404).json({message:'ID продукта не указан'});
-            const product = await Product.findByIdAndDelete(id);
-            return res.json(product);
-        } catch (error) {
-          res.status(500).json(error);
-        }
-      }
+    try {
+      const product = req.body;
+      if (!product.id)
+        return res.status(404).json({ message: "ID продукта не указан" });
+      const updatedProduct = await Post.findByIdAndUpdate(product.id, product, {
+        new: true,
+      });
+      return res.json(updatedProduct);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   }
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id)
+        return res.status(404).json({ message: "ID продукта не указан" });
+      const product = await Product.findByIdAndDelete(id);
+      return res.json(product);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+}
 
 export default new ProductController();
